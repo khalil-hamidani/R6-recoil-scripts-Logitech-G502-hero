c9b4845516c88e26272570f39ddfe4cff26c38a5
khalil-hamidani <hamidani2002@gmail.com>
2023-08-10 01:12:00 +0100

    first commit

---
+++ OSA.lua
@@ -0,0 +1,16 @@
+EnablePrimaryMouseButtonEvents  (true);
+local agent = 6
+function OnEvent(event,arg)
+    if IsKeyLockOn("capslock")then
+        if IsMouseButtonPressed(3)then
+            repeat
+                if IsMouseButtonPressed(1) then
+                    repeat
+                        MoveMouseRelative(0,agent)
+                        Sleep(9)
+                    until not IsMouseButtonPressed(1)
+                end
+            until not IsMouseButtonPressed(3)
+        end
+    end
+end
